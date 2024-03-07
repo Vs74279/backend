5921cf1a2b274deaa0d03f364dce19a5b0f2c1ce
vikash kumar singh <drv74279@gmail.com>
2024-03-07 17:15:06 +0530

    fetching all notes & adding a note

--- models/User.js
+++ models/User.js
@@ -1,26 +1,29 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 
 const UserSchema = new Schema({
-    
-    email:{
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
         type: String,
         required: true,
         unique: true
-        
+
     },
-    password:{
+    password: {
         type: String,
         required: true
 
     },
-    date:{
+    date: {
         type: Date,
         default: Date.now
     }
-  });
-  const User = mongoose.model('user',UserSchema);
-  
-  module.exports = User;
-  
+});
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,57 @@
 const express = require('express');
 const router = express.Router();
+var fetchuser = require('../middleware/fetchuser');
+const Notes = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    
-    res.json([])
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+
+        const notes = await Notes.find({ user: req.user.id });
+
+        res.json(notes)
+
+    } catch (error) {
+
+        console.error(error.message);
+        res.status(500).send("Internal server error ");
+    }
+
+
 })
+
+router.post('/addnote', fetchuser, [
+
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', ' description must be  atleast 5 characters').isLength({ min: 5 }),
+
+
+], async (req, res) => {
+
+    try {
+
+
+
+
+        const { title, description, tag } = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save()
+
+
+        res.json(savedNote)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error ");
+
+    }
+
+})
+
 module.exports = router
