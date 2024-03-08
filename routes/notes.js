ecd418365d44475a7058035d285b8e121662f56e
vikash kumar singh <drv74279@gmail.com>
2024-03-08 12:21:48 +0530

    Endpoint for deleting a Note

--- routes/notes.js
+++ routes/notes.js
@@ -54,26 +54,53 @@ router.post('/addnote', fetchuser, [
 
 })
 
-router.put('/updatenote/:id', fetchuser,  async (req, res) => {
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
 
-    const {title,description,tag} = req.body;
-    const newNote = {};
-    if(title){newNote.title = title};
-    if(description){newNote.description = description};
-    if(tag){newNote.tag = tag};
+    const { title, description, tag } = req.body;
+    try {
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
 
-    let note = await  Notes.findById(req.params.id);
-    if(!note){res.status(404).send("not found")}
+        let note = await Notes.findById(req.params.id);
+        if (!note) { res.status(404).send("not found") }
 
-    if(note.user.toString() !== req.user.id){
-        return res.status(401).send("Not Allow");
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allow");
+        }
+
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error ");
+
     }
 
-    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
-    res.json({note});
-
-
-
 })
+
+
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+
+    try {
+        let note = await Notes.findById(req.params.id);
+        if (!note) { res.status(404).send("not found") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allow");
+        }
+
+        note = await Notes.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deteled", note: note });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error ");
+
+    }
+})
+
 
 module.exports = router
